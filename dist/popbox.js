864218bf053eb1779faf63c726e3889863b23f5b
Troy Whiteley <troy@somanyscientists.com>
2013-04-28 16:08:28 -0700

    First version. Contains the following features:
     - passable direction through config or data-popbox-direction
     - allows mouse to hover over tooltip without it going away
     - ability to use a custom tooltip template
     - ability to enable/disable hover and click to show
     - events on show/hide

--- dist/popbox.js
+++ dist/popbox.js
@@ -7,6 +7,162 @@
 */
 (function($) {
   $.declare('popbox', {
+    defaults: {
+      containerClass: 'Popbox',
+      direction: 'left',
+      hideFadeDuration: 500,
+      showFadeDuration: 50,
+      hideTimeout: 500, 
+      enableHover: true,
+      clickToShow: true
+    },
 
+    init: function () {
+      this.open = false;
+      this.hoveringOverTooltip = false;
+      this.hideTimer = null;
+
+      this.text = this.el.data('popbox-text') || '';
+      this.title = this.el.data('popbox-title') || '';
+      this.templateEl = this.el.data('popbox-el') || '';
+      this.direction = this.el.data('popbox-direction') || this.direction;
+      this.template = '';
+
+      this.attachEvents();
+    },
+
+    isOpen: function () {
+      return this.open;
+    },
+
+    attachEvents: function () {
+      if(this.enableHover) {
+        this.el.bind('mouseenter', this.proxy(this.show));
+        this.el.bind('mouseleave', this.proxy(this.hide));
+      }
+    },
+
+    reset: function () {
+      this.template.unbind('mouseenter');
+      this.template.unbind('mouseleave');
+      this.template.remove();
+    },
+
+    show: function () {
+      if(this.hoveringOverTooltip) return;
+
+      clearTimeout(this.hideTimer);
+
+      if(!this.open) {
+        $('body').append(this.generateTemplate());
+
+        this.template.bind('mouseenter', this.proxy(this.hoverTooltip));
+        this.template.bind('mouseleave', this.proxy(this.hoverLeaveTooltip));
+
+        this.template.hide().fadeIn(this.showFadeDuration);
+        this.position();
+      }
+
+      this.open = true;
+      this.el.trigger('show');
+    },
+
+    hide: function () {
+      if(this.hoveringOverTooltip) return;
+
+      if(!this.open) return;
+
+      this.hideTimer = setTimeout(this.proxy(function() {
+        this.open = false;
+        this.template.fadeOut(this.hideFadeDuration, this.proxy(this.reset));
+        this.el.trigger('hide');
+      }), this.hideTimeout);
+    },
+
+    toggle: function () {
+      if(!this.clickToShow) return;
+
+      if(this.open) {
+        this.hide();
+      } else {
+        this.show();
+      }
+
+      return false;
+    },
+
+    position: function () {
+      this.template.css({
+        position: 'absolute'
+      });
+
+      var elOffset = this.el.offset();
+
+      switch(this.direction) {
+        case 'left':
+          this.template.css({
+            left: elOffset.left - this.template.outerWidth(),
+            top: (elOffset.top + this.el.outerHeight() / 2) - (this.template.outerHeight() / 2)
+          });
+          break;
+        case 'up':
+          this.template.css({
+            left: (elOffset.left + this.el.outerWidth() / 2) - (this.template.outerWidth() / 2),
+            top: elOffset.top  - this.template.outerHeight()
+          });
+          break;
+        case 'right':
+          this.template.css({
+            left: elOffset.left + this.el.outerWidth(),
+            top: (elOffset.top + this.el.outerHeight() / 2) - (this.template.outerHeight() / 2)
+          });
+          break;
+      case 'down':
+          this.template.css({
+            left: (elOffset.left + this.el.outerWidth() / 2) - (this.template.outerWidth() / 2),
+            top: elOffset.top  + this.el.outerHeight()
+          });
+          break;
+      }
+    },
+
+    generateTemplate: function () {
+      if(this.template) return this.template;
+
+      var container = $(document.createElement('div'));
+      var title = $(document.createElement('div'));
+      var text = $(document.createElement('div'));
+
+      container.addClass(this.containerClass);
+
+      if(this.templateEl) {
+        container.append($(this.templateEl).html());
+      } else {
+        if(this.title) {
+          title.addClass('title');
+          title.html(this.title);
+          container.append(title);
+        }
+
+        if(this.text) {
+          text.addClass('text');
+          text.html(this.text);
+          container.append(text);
+        }
+      }
+
+      this.template = container;
+      return this.template;
+    },
+
+    hoverTooltip: function () {
+      clearTimeout(this.hideTimer);
+      this.hoveringOverTooltip = true;
+    },
+
+    hoverLeaveTooltip: function () {
+      this.hoveringOverTooltip = false;
+      this.el.trigger('mouseleave');
+    }
   });
 })(jQuery);
--- test/popbox.test.js
+++ test/popbox.test.js
@@ -1,4 +1,129 @@
+$.fn.popbox.defaults.hideTimeout = 10;
 
 suite('popbox', function() {
+  var tooltip,
+      tooltipTitle,
+      tooltipCustom;
+
+  setup(function() {
+    tooltip = $('.hover-popbox');
+    tooltipTitle = $('.hover-popbox-title');
+    tooltipCustom = $('.custom-popbox');
+  });
+
+  suite('init', function() {
+
+    test('$().popbox exists', function() {
+      assert.equal(typeof tooltip.popbox, 'function');
+    });
+  });
+
+  suite('generateTemplate', function() {
+    test('template without title', function() {
+      tooltip.popbox();
+
+      assert.equal(tooltip.popbox('generateTemplate')[0].outerHTML, '<div class="Popbox"><div class="text">This is a tooltip</div></div>');
+    });
+
+    test('template with title', function() {
+      tooltipTitle.popbox();
+
+      assert.equal(tooltipTitle.popbox('generateTemplate')[0].outerHTML, '<div class="Popbox"><div class="title">Awesome title</div><div class="text">This is a tooltip</div></div>');
+    });
+
+    test('custom template', function() {
+      tooltipCustom.popbox();
+
+      assert.equal(tooltipCustom.popbox('generateTemplate')[0].innerHTML, $('.custom-popbox-content')[0].innerHTML);
+    });
+  });
+
+  suite('show', function() {
+    test('show on mouseenter', function() {
+      tooltip.popbox();
+
+      tooltip.mouseenter();
+
+      assert.equal(tooltip.popbox('isOpen'), true);
+    });
+
+    test('show on click', function() {
+      tooltip.popbox();
+
+      tooltip.click();
+
+      assert.equal(tooltip.popbox('isOpen'), true);
+    });
+
+    test('show manually', function() {
+      tooltip.popbox();
+
+      tooltip.popbox('show');
+
+      assert.equal(tooltip.popbox('isOpen'), true);
+    });
+
+    test('show event triggered', function(done) {
+      tooltip.popbox();
+
+      tooltip.one('show', function() {
+        assert.ok(true);
+        done();
+      });
+
+      tooltip.popbox('show');
+    });
+  });
+
+  suite('hide', function() {
+    test('hide on mouseleave', function(done) {
+      tooltip.popbox();
+
+      tooltip.mouseenter();
+      tooltip.mouseleave();
+
+      setTimeout(function() {
+        assert.notEqual(tooltip.popbox('isOpen'), true);
+        done();
+      }, 20);
+    });
+
+    test('hide on click', function(done) {
+      tooltip.popbox();
+
+      tooltip.click();
+      tooltip.click();
+
+      setTimeout(function() {
+        assert.notEqual(tooltip.popbox('isOpen'), true);
+        done();
+      }, 20);
+    });
+
+    test('hide manually', function(done) {
+      tooltip.popbox();
+
+      tooltip.popbox('show');
+      tooltip.popbox('hide');
+
+      setTimeout(function() {
+        assert.notEqual(tooltip.popbox('isOpen'), true);
+        done();
+      }, 20);
+
+    });
+
+    test('hide event triggered', function(done) {
+      tooltip.popbox();
+
+      tooltip.one('hide', function() {
+        assert.ok(true);
+        done();
+      });
+
+      tooltip.popbox('show');
+      tooltip.popbox('hide');
+    });
+  });
 
 });
