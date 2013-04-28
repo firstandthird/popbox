0970174bb9af09190f2d4ca0f2a2ad5d5482ceaa
Greg Allen <me@jga.me>
2013-04-28 08:23:26 -0700

    initial commit

---
+++ dist/popbox.js
@@ -0,0 +1,12 @@
+/*!
+ * popbox - Fidel Tooltip/Popover Library
+ * v0.1.0
+ * https://github.com/jgallen23/popbox
+ * copyright Greg Allen 2013
+ * MIT License
+*/
+(function($) {
+  $.declare('popbox', {
+
+  });
+})(jQuery);
