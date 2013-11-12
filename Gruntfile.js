75fbec3a4897a38efdf1d2b0e0282496840734b9
Greg Allen <me@jga.me>
2013-11-11 21:46:53 -0800

    many fixes.  fixes #7. fixes #6. fixes #5. fixes #3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,138 +1,15 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    info: grunt.file.readJSON('bower.json'),
-    meta: {
-      banner: '/*!\n'+
-              ' * <%= info.name %> - <%= info.description %>\n'+
-              ' * v<%= info.version %>\n'+
-              ' * <%= info.homepage %>\n'+
-              ' * copyright <%= info.copyright %> <%= grunt.template.today("yyyy") %>\n'+
-              ' * <%= info.license %> License\n'+
-              '*/\n'
-    },
-    jshint: {
-      main: [
-        'Gruntfile.js', 
-        'bower.json',
-        'lib/**/*.js',
-        'test/*.js'
-      ]
-    },
-    bower: {
-      main: {
-        dest: 'dist/_bower.js',
-        exclude: [
-          'jquery',
-          'assert'
-        ]
-      }
-    },
-    concat: {
-      options: {
-        banner: '<%= meta.banner %>'
-      },
-      dist: {
-        src: [
-          'lib/popbox.js'
-        ],
-        dest: 'dist/fidel.popbox.js'
-      },
-      full: {
-        src: [
-          'dist/_bower.js',
-          'lib/popbox.js'
-        ],
-        dest: 'dist/popbox.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= meta.banner %>'
-      },
-      dist: {
-        src: 'dist/fidel.popbox.js',
-        dest: 'dist/fidel.popbox.min.js'
-      },
-      full: {
-        src: 'dist/popbox.js',
-        dest: 'dist/popbox.min.js'
-      }
-    },
-    clean: {
-      bower: [
-        'dist/_bower.js'
-      ],
-      dist: [
-        'dist'
-      ]
-    },
-    less: {
-      styles: {
-        files: {
-          'dist/popbox.css': 'lib/default.less'
-        }
-      }
-    },
-    watch: {
-      main: {
-        files: '<%= jshint.main %>',
-        tasks: 'scripts' 
-      },
-      grunt: {
-        files: [
-          'Gruntfile.js',
-          'test/index.html'
-        ],
-        tasks: 'default'
-      },
-      styles: {
-        files: 'lib/*.less',
-        tasks: ['styles']
-      }
-    },
-    mocha: {
-      options: {
-        run: true,
-        growl: true,
-        reporter: 'Spec'
-      },
-      all: {
-        src: 'test/index.html'
-      }
-    },
-    plato: {
-      main: {
-        files: {
-          'reports': ['lib/*.js']
-        }
-      }
-    },
-    connect: {
-      server:{
-        options: {
-          hostname: '*'
-        }
-      },
-      plato: {
-        port: 8000,
-        base: 'reports',
-        options: {
-          keepalive: true
-        }
-      }
-    },
-    bytesize: {
-      scripts: {
-        src: [
-          'dist/*'
-        ]
-      }
+
+  require('load-grunt-config')(grunt, {
+    config: {
+      info: grunt.file.readJSON('bower.json'),
+      name: 'popbox'
     }
   });
-  require('load-grunt-tasks')(grunt);
-  grunt.registerTask('styles', ['less']);
-  grunt.registerTask('scripts', ['jshint', 'bower', 'concat', 'uglify', 'clean:bower', 'mocha', 'bytesize']);
-  grunt.registerTask('default', ['styles', 'scripts']);
-  grunt.registerTask('dev', ['connect:server', 'watch']);
-  grunt.registerTask('reports', ['plato', 'connect:plato']);
+
+  grunt.registerTask('script-dist', ['concat:dist', 'uglify:dist']);
+  grunt.registerTask('script-full', ['concat:full', 'uglify:full']);
+  grunt.registerTask('scripts', ['jshint', 'bower', 'script-dist', 'script-full', 'clean:bower', 'mocha', 'bytesize', 'notify:generic']);
+  grunt.registerTask('default', ['scripts', 'less']);
+  grunt.registerTask('dev', ['default', 'connect:server', 'notify:watch', 'watch']);
 };
